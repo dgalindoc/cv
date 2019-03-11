1f1388772a5482cd953ed695246aae7080836a14
david <dgalindoc@gmail.com>
2019-03-11 16:21:20 +0100

    Deploy to gh-pages

--- next.config.js
+++ next.config.js
@@ -5,6 +5,10 @@ module.exports = compose([
   [withSass],
   {
     assetPrefix: '/cv',
+    webpack: (config) => {
+    config.output.publicPath = `/cv${config.output.publicPath}`;
+    return config;
+    },
     exportPathMap: function () {
       return {
         '/': { page: '/' }
